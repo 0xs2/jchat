05b6e7cecaf32dd0be4a7ecffa2da31c6152b2c0
0x <sui@0xs.dev>
2023-12-19 10:55:57 +0000

    stupid

--- src/main/java/dev/oxs/jchat/commands/JChatCommand.java
+++ src/main/java/dev/oxs/jchat/commands/JChatCommand.java
@@ -26,27 +26,4 @@ public class JChatCommand implements CommandExecutor {
             return true;
         }
     }
-
-    public static class JChatMuteCommand implements CommandExecutor {
-
-        private final Plugin plugin;
-
-        public JChatMuteCommand(Plugin plugin) {
-            this.plugin = plugin;
-        }
-
-        @Override
-        public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-            if (!(commandSender.hasPermission("jchat.jchat") || commandSender.isOp())) {
-                commandSender.sendMessage(ChatColor.RED + "No permission.");
-                return true;
-            } else {
-                commandSender.sendMessage("Version : " + ChatColor.BLUE + "v" + plugin.getDescription().getVersion());
-                commandSender.sendMessage("Description : " + ChatColor.BLUE + plugin.getDescription().getDescription());
-                commandSender.sendMessage("Author(s) : " + ChatColor.BLUE + plugin.getDescription().getAuthors().toString());
-                return true;
-            }
-        }
-
-    }
 }
